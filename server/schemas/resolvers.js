74d73a06679cac63530456b40e2f9176606a4c26
Jacob Banks <kobybanks@gmail.com>
2021-10-06 17:02:55 -0400

    auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { User, Thought } = require("../models");
-
+const { signToken } = require("../utils/auth");
 const resolvers = {
   Query: {
     thoughts: async (parent, { username }) => {
@@ -25,8 +25,29 @@ const resolvers = {
     },
   },
   Mutation: {
-    addUser: async () => {},
-    login: async () => {},
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
   },
 };
 
